9141c920a9d2a99c545e11e4d991c80098c456e7
Najeeb-Sulaiman <beejan003@gmail.comgit config --global user.name Najeeb-Sulaimangit commit -m first>
2020-02-11 16:58:54 +0100

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyC3iToTC3vtUou1npjXvqQmlVjGr8ypQj0",
+    authDomain: "todolistapp-94305.firebaseapp.com",
+    databaseURL: "https://todolistapp-94305.firebaseio.com",
+    projectId: "todolistapp-94305",
+    storageBucket: "todolistapp-94305.appspot.com",
+    messagingSenderId: "1023353889488",
+    appId: "1:1023353889488:web:e3cdc9792420d59603b4d5",
+    measurementId: "G-G83L75XN4J"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
